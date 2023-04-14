4198356d3c324e338ccd71038c1d8f8157d50baf
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-04-14 17:41:35 -0300

    feat: testing header home

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("home page", () => {
+  beforeEach(() => {
+    //This is a function that will get called “before each” test is run
+    cy.visit("http://localhost:3000")
+  })
+
+  it("the h1 contains the correct text", () => {
+    cy.get("[data-test='hero-heading']").contains(
+      "Testing Next.js Applications with Cypress"
+    )
+  })
+
+  //Notice that within each test, we have to specifically tell Cypress where to navigate to in our application.
+
+  it("the features on the homepage are correct", () => {
+    cy.get("dt").eq(0).contains("4 Courses") // case-sensitive
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
   })
 })
